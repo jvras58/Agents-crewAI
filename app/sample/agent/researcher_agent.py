6527b8a64cbe5ba0be9193ab213ef2f31f127bcf
jvras <jonathas.silva@viitra.com>
2025-10-20 19:24:14 -0300

    Atualiza a definição do Agente Pesquisador e corrige a importação da função de construção de prompt

--- app/sample/agent/researcher_agent.py
+++ app/sample/agent/researcher_agent.py
@@ -1,14 +1,17 @@
 """
 Definição do Agente Pesquisador.
-Responsável por buscar contexto usando o RagTool.
 """
 from crewai import LLM, Agent
 from crewai_tools import RagTool
 
-from utils.config.prompt_builder import build_agent_prompt_by_name
+from utils.config.prompt_builder import build_prompt_by_name
 from utils.settings import get_settings
 
 
+# FIXME: Acredito que esteja fazendo isso errado, revisar depois
+# pois temos duas maneiras de usar Rag no crewai:
+# https://docs.crewai.com/en/tools/overview ( Que seria o rag_tool_json )
+# Ou https://docs.crewai.com/en/tools/ai-ml/ragtool ( Que seria o from crewai_tools import RagTool )
 def get_researcher_agent(llm: LLM, rag_tool: RagTool) -> Agent:
     """
     Cria e retorna o Agente Pesquisador.
@@ -17,13 +20,12 @@ def get_researcher_agent(llm: LLM, rag_tool: RagTool) -> Agent:
         llm: A instância da LLM a ser usada.
         rag_tool: A instância da RagTool configurada.
     """
-    prompt = build_agent_prompt_by_name("researcher_agent", "app/sample/agent/prompt")
+    prompt = build_prompt_by_name("researcher_agent", "app/sample/agent/prompt")
 
     return Agent(
         role=prompt["role"],
         goal=prompt["goal"],
         backstory=prompt["backstory"],
-        # tools=[rag_tool], # TODO: Reativar quando tivermos uma chave embedding funcional (Google ou gpt)_
         tools=[],
         llm=llm,
         max_iter=3,
